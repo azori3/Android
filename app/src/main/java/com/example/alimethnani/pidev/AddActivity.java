f1e2ff2410e84bfc94bf5f38d4f22f13757cea87
azori3 <ali.methnani@esprit.tn>
2017-11-27 23:55:14 +0100

    second commit

--- app/src/main/java/com/example/alimethnani/pidev/AddActivity.java
+++ app/src/main/java/com/example/alimethnani/pidev/AddActivity.java
@@ -8,7 +8,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+            setContentView(R.layout.activity_add);
 
                }
 
